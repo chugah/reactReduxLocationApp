e0e4e0a7c24c651c2c3484e41137e86c081f7863
Sami Barghshoon <chugah@users.noreply.github.com>
2016-09-15 16:57:46 -0400

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+	name: 'Anonymous',
+	hobbies: [],
+	movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
 	// state = state || {name: 'Anonymous'};
 
 	switch (action.type) {
@@ -11,6 +19,44 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
 				...state,
 				name: action.name
 			};
+		
+		case 'ADD_HOBBY':
+			return {
+				...state,
+				hobbies: [
+					...state.hobbies,
+					{
+						id: nextHobbyId++,
+						hobby: action.hobby
+					}
+				]
+			};
+
+		case 'REMOVE_HOBBY':
+			return {
+				...state,
+				hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)			
+			};
+
+		case 'ADD_MOVIE':
+			return {
+				...state,
+				movies: [
+					...state.movies,
+					{
+						id: nextMovieId++,
+						title: action.title,
+						genre: action.genre
+					}
+				]
+			};
+
+		case 'REMOVE_MOVIE':
+			return {
+				...state,
+				movies: state.movies.filter((movie) => movie.id !== action.id)			
+			};
+
 		default:
 			return state;
 	}
@@ -26,6 +72,8 @@ var unsubscribe = store.subscribe( () => {
 
 	console.log('Name is ', state.name);
 	document.getElementById('app').innerHTML = state.name;
+
+	console.log('New state', store.getState());
 });
 
 // unsubscribe();
@@ -38,9 +86,40 @@ store.dispatch ({
 	name: 'Jennifer'
 });
 
+store.dispatch ({
+	type: 'ADD_HOBBY',
+	hobby: 'Running'
+});
 
+store.dispatch ({
+	type: 'ADD_HOBBY',
+	hobby: 'Walking'
+});
+
+store.dispatch ({
+	type: 'REMOVE_HOBBY',
+	id: 2
+});
+
+store.dispatch ({
+	type: 'ADD_MOVIE',
+	title: 'Jaws',
+	genre: 'Action'
+});
+
+store.dispatch ({
+	type: 'REMOVE_MOVIE',
+	id: 1
+});
 
 store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Emily'
 });
+
+store.dispatch ({
+	type: 'ADD_MOVIE',
+	title: 'Star Wars',
+	genre: 'Sci Fi'
+});
+
