3430311a2b6bafe0d8c0caf6a5dba8afcf7538e3
Sami Barghshoon <chugah@users.noreply.github.com>
2016-09-19 14:26:02 -0400

    Refactor files to create separate files for actions, reducers, and store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,149 +3,8 @@ var axios = require('axios');
 
 console.log('Starting redux example');
 
-// Name reducer and action generators
-// --------------------
-var nameReducer = (state = 'Anonymous', action) => {
-	switch (action.type) {
-		case 'CHANGE_NAME': 
-			return action.name
-		default:
-			return state;
-	};
-};
-
-var changeName = (name) => {
-	return {
-		type: 'CHANGE_NAME',
-		name
-	}
-};
-
-// Hobby reducer and action generators
-// --------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_HOBBY': 
-			return [
-					...state,
-					{
-						id: nextHobbyId++,
-						hobby: action.hobby
-					}
-				];
-		case 'REMOVE_HOBBY':
-			return state.filter((hobby) => hobby.id !== action.id)						
-		default:
-			return state;
-	}
-};
-
-var addHobby = (hobby) => {
-	return {
-		type: 'ADD_HOBBY',
-		hobby
-	}
-};
-
-var removeHobby = (id) => {
-	return {
-		type: 'REMOVE_HOBBY',
-		id
-	}
-};
-
-// Movie reducer and action generators
-// --------------------
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_MOVIE': 
-			return [
-					...state,
-					{
-						id: nextMovieId++,
-						title: action.title,
-						genre: action.genre
-					}
-				];
-		case 'REMOVE_MOVIE':
-			return state.filter((movie) => movie.id !== action.id)						
-		default:
-			return state;
-	}
-};
-
-var addMovie = (title, genre) => {
-	return {
-		type: 'ADD_MOVIE',
-		title,
-		genre
-	}
-};
-
-var removeMovie = (id) => {
-	return {
-		type: 'REMOVE_MOVIE',
-		id
-	}
-};
-
-// Map reducer and action generators
-// --------------------
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-	switch(action.type) {
-		case 'START_LOCATION_FETCH':
-			return {
-				isFetching: true,
-				url: undefined
-			};
-		case 'COMPLETE_LOCATION_FETCH':
-			return {
-				isFetching: false,
-				url: action.url
-			};
-		default: 
-			return state;
-	}
-};
-
-var startLocationFetch = () => {
-	return {
-		type: 'START_LOCATION_FETCH'
-	};
-};
-
-var completeLocationFetch = (url) => {
-	return {
-		type:'COMPLETE_LOCATION_FETCH',
-		url
-	};
-};
-
-var fetchLocation = () => {
-	store.dispatch(startLocationFetch());
-
-	axios.get('http://ipinfo.io').then(function (res) {
-		var loc = res.data.loc;
-		var baseUrl = 'http://maps.google.com?q=';
-
-		store.dispatch(completeLocationFetch(baseUrl + loc));
-	});
-};
-
-// Reducer & store functions
-var reducer = redux.combineReducers ({
-	name: nameReducer,
-	hobbies: hobbiesReducer,
-	movies: moviesReducer,
-	map: mapReducer
-})
-
-var store = redux.createStore(reducer, redux.compose(
-	window.devToolsExtension ? window.devToolsExtension() : f => f
-	)
-);
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
 // Subscribe to changes
 var unsubscribe = store.subscribe( () => {
@@ -168,18 +27,18 @@ var unsubscribe = store.subscribe( () => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 
-store.dispatch(changeName('Jennifer'));
+store.dispatch(actions.changeName('Jennifer'));
 
-store.dispatch(addHobby('Running'));
-store.dispatch(addHobby('Walking'));
-store.dispatch(removeHobby(2));
+store.dispatch(actions.addHobby('Running'));
+store.dispatch(actions.addHobby('Walking'));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch(addMovie('Jaws', 'Action'));
-store.dispatch(removeMovie(1));
+store.dispatch(actions.addMovie('Jaws', 'Action'));
+store.dispatch(actions.removeMovie(1));
 
-store.dispatch(changeName('Emily'));
+store.dispatch(actions.changeName('Emily'));
 
-store.dispatch(addMovie('Star Wars', 'Sci Fi'));
+store.dispatch(actions.addMovie('Star Wars', 'Sci Fi'));
 
